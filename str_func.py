efadf24a235d37cc7e22f669ca9f806f2611380a
KibalinMax <kibalaspb@mail.ru>
2023-05-25 20:20:52 +0300

    Add src_func change

--- str_func.py
+++ str_func.py
@@ -1,6 +1,11 @@
 def title_name():
     """
-    Функция делает все буквы в строке заглавными
+    Функция делает все буквы строки заглавными
+    :return: None
     """
     user_input = input()
     print(user_input.lower())
+
+def swapcase():
+    user_input_1 = input()
+    print(user_input_1.swapcase())
